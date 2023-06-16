2009967fd0fd9ee7fbc8ff53ced6758399862e97
waffleavenue <44412878+waffleavenue@users.noreply.github.com>
2023-06-16 09:53:19 +0200

    Updated weather parser
    Formatted output, config input file, delay 1h

--- weather-parser/weather_parser.py
+++ weather-parser/weather_parser.py
@@ -1,16 +1,24 @@
-import requests
-import json 
-import time 
-import csv
+import requests, json, time, csv, time, os 
 
 
-CSV_name = "weatherdata.csv"
-URL_base = "http://api.weatherapi.com/v1"
-api_key = "e6f2e34add2d1ba268283797394bc32f"
-csv_file = "weather.csv"
-time_delay = 3600 # every hour
+# only TU Berlin as location
 lat = 52.5119
 lon = 13.3262
+
+
+#Load config data
+config_file = 'config.json'
+
+# Read the data from the config file
+with open(config_file) as file:
+    config_data = json.load(file)
+
+# Access the data from the config file
+URL_base = config_data['URL_base']
+api_key = config_data['api_key']
+csv_file = config_data['csv_file']
+time_delay = config_data['time_delay']
+
 
 def get_current_weather():
     try:
@@ -23,25 +31,36 @@ def get_current_weather():
                 "lat":lat,
                 "lon":lon,
                 "timezone":result["timezone"],
-                "timestamp":int(time.time()),
-                "current_data":result["current"]
+                "sunrise_unix":result["current"]["sunrise"],
+                "sunset_unix":result["current"]["sunset"],
+                "temp":result["current"]["temp"],
+                "pressure":result["current"]["pressure"],
+                "humidity":result["current"]["humidity"],
+                "wind_speed":result["current"]["wind_speed"],
+                "weather_naming":result["current"]["weather"][0]["description"],
+                "timestamp":int(time.time())
             }
-            print("[weather-parser] - Successfully parsed weather [{0},{1}]".format(lat, lon))
             append_json_to_csv(resp_obj)
         else:
             raise Exception ("Error parsing data - {}".format(s.status_code))
-        
     except Exception as e:
         print("[weather-parser] - Error [{0}]".format(e))
 
+
 def append_json_to_csv(json_data):
+    if not os.path.isfile(csv_file):
+        with open(csv_file, 'w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=json_data.keys())
+            writer.writeheader()
+
     with open(csv_file, 'a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([json.dumps(json_data)])
-        print("[weather-parser] - Successfully inserted into {}".format(csv_file))
+        writer = csv.DictWriter(file, fieldnames=json_data.keys())
+        writer.writerow(json_data)
+    print("[weather-parser] - Successfully parsed weather [{0},{1}] and inserted into {2}".format(lat, lon, csv_file))
+
+
 
 if __name__ == "__main__":
     while True:
         get_current_weather()
-        #parse every hour
         time.sleep(time_delay)
