38dd0dd1a87178224f13e76dcfa45d9c1b42aea7
waffleavenue <tim.michaelis@campus.tu-berlin.de>
2023-07-07 10:25:56 +0200

    Updated sensors
    switch from live to simulated env data

--- edge-device/sensor_1.py
+++ edge-device/sensor_1.py
@@ -1,6 +1,17 @@
 import random
 import time
 import json
+import requests
+
+
+# Read the data from the config file
+with open("config.json") as file:
+    config_data = json.load(file)
+api_key = config_data['api_key']
+simulate_data = config_data['simulate_data']
+generate_data = simulate_data.lower() == 'true'
+
+
 
 class SensorData_1:
     def __init__(self):
@@ -13,28 +24,57 @@ class SensorData_1:
         self.cities = ['Berlin', 'Hamburg', 'Munich']
 
     def generate_sensor_data(self):
-        current_time = int(time.time())
-        time_measurement = current_time 
-        temperature = round(random.uniform(*self.temperature_range), 1)
-        pressure = round(random.uniform(*self.pressure_range), 1)
-        humidity = random.randint(*self.humidity_range)
-        wind_speed = round(random.uniform(*self.wind_speed_range), 1)
-        weather_naming = random.choice(self.weather_naming_options)
-        timestamp_request = time_measurement + random.randint(1, 10)
-        # city = random.choice(self.cities)
+        if(generate_data == True):
+            print(f"[weather-parser] - Simulating environmental data for [{self.cities[0]}]")
+            current_time = int(time.time())
+            time_measurement = current_time 
+            temperature = round(random.uniform(*self.temperature_range), 1)
+            pressure = round(random.uniform(*self.pressure_range), 1)
+            humidity = random.randint(*self.humidity_range)
+            wind_speed = round(random.uniform(*self.wind_speed_range), 1)
+            weather_naming = random.choice(self.weather_naming_options)
+            timestamp_request = time_measurement + random.randint(1, 10)
 
-        sensor_entry = {
-            'city': self.cities[0],
-            'country': 'Germany',
-            'time_of_measurement': time_measurement,
-            'temperature': temperature,
-            'pressure': pressure,
-            'humidity': humidity,
-            'wind_speed': wind_speed,
-            'weather_naming': weather_naming,
-            'timestamp_request': timestamp_request
-        }
-        return sensor_entry
+
+            sensor_entry = {
+                'city': self.cities[0],
+                'country': 'Germany',
+                'time_of_measurement': time_measurement,
+                'temperature': temperature,
+                'pressure': pressure,
+                'humidity': humidity,
+                'wind_speed': wind_speed,
+                'weather_naming': weather_naming,
+                'timestamp_request': timestamp_request
+            }
+            return sensor_entry
+        else:
+            try:
+                print(f"[weather-parser] - Parsing environmental data from API for [{self.cities[0]}]")
+                city = self.cities[0]
+                url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}&aqi=no"
+                x = requests.Session()
+                s = x.get(url)
+                if(s.status_code == 200):
+                    result = json.loads(s.text)
+                    resp_obj = {
+                        "city":city,
+                        "country":result["location"]["country"],
+                        "time_of_measurement":result["location"]["localtime_epoch"],
+                        "temperature":result["current"]["temp_c"],
+                        "pressure":result["current"]["pressure_mb"],
+                        "humidity":result["current"]["humidity"],
+                        "wind_speed":result["current"]["wind_kph"],
+                        "weather_naming":result["current"]["condition"]["text"],
+                        "timestamp_request":int(time.time())
+                    }
+                    return resp_obj
+                else:
+                    raise Exception ("Error parsing data - {}".format(s.status_code))
+            except Exception as e:
+                print("[weather-parser] - Error [{0}]".format(e))
+                return None
+
 
 
 # Example usage:
