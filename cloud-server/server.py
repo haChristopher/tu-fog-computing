112f93c40d09af6e9fd9de043c887a98e13124fc
chansen113 <christopher.hansen.1@campus.tu-berlin.de>
2023-07-08 12:48:10 +0200

    (add) adds reliable messaging

--- cloud-server/server.py
+++ cloud-server/server.py
@@ -1,45 +1,75 @@
 import sys
 import os
 import json
-import time
+import random
 import threading
 import requests
 import logging as log
 
 import zmq
 
+
+# Read environment variables
+URL_CLIENT = os.getenv("SERVER_ADDRESS", default="tcp://*:5555")
+BACKEND_ENDPOIMT = os.getenv("SERVER_ENDPOINT", default="http://127.0.0.1:5000")
+SERVER_ID = os.getenv("SERVER_ID", default=random.randint(0, 1000000))
+
 # Setup logging
 log.basicConfig(stream=sys.stdout, level=log.DEBUG)
 
-URL_CLIENT = os.getenv("SERVER_ADDRESS", default="tcp://*:5555")
-BACKEND_ENDPOIMT = os.getenv("SERVER_ENDPOINT", default="http://127.0.0.1:5000")
 
+"""Calculate information for edge device to send back to edge device"""
+def getInformationForEdgeDevice(device_id: int) -> object:
+    return {
+        "id": SERVER_ID,
+        "ack": True,
+        "warnings": [],
+        "alerts": [
+            {
+                "id": 1, 
+                "message": "STORM !!!! PANIC",
+                "time": "2020-12-12 12:12:12",
+                "level": "HIGH"
+            }
+        ],
+        "maintenance_schedule": [],
+        "weather_forecast": [],
+    }
+
+
+"""Send sensor data to flask endpoint to store in DB"""
 def post_to_flask(json_data):
     try:
         full_endpoint = BACKEND_ENDPOIMT + "/api/v2/submit"
         requests.post(full_endpoint, json=json_data)
-        log.info("Sent sensor data to flask: % s" % json_data)
+        # log.info("Sent sensor data to flask: % s" % json_data)
     except Exception as e:
         # TODO: Handle errors
         log.error(e)
 
+"""Receive messages from edge device and send to flask"""
 def msg_receiver():
+    count = 0
     log.debug("Server is starting ...")
 
     context = zmq.Context()
     socket = context.socket(zmq.REP)
-    # socket.setsockopt(zmq.HWM, 0)
     socket.bind(URL_CLIENT)
 
     log.debug("Waiting for messages ...")
     while True:
         # receive sensor data
         data = socket.recv_pyobj()
-        log.info("Received sensor data from client: % s" % data)
+        # log.info("Received sensor data from client: % s" % data)
+        
+        response_obj = getInformationForEdgeDevice(3)
         socket.send_string("ACK")
+        count += 1
+        log.info(f"Received Messages: {count}")
+        # socket.send_pyobj(response_obj)
         
         # send sensor data to flask
-        post_to_flask(json.dumps(data))
+        # post_to_flask(json.dumps(data))
 
     
 
--- edge-device/client.py
+++ edge-device/client.py
@@ -6,11 +6,11 @@ All Patter: http://api.zeromq.org/2-1%3azmq-socket#toc13
 
 TODO When server is is off client just stops and block it should trow and error
 TODO Disable conenction handling
-TODO add queue with sender
 """
 import os
 import sys
 import json
+import queue
 import time
 import random
 import threading
@@ -25,84 +25,121 @@ from sensor_3 import SensorData_3
 import sqlite_helper as db_helper
 from exceptions import NoAckException
 
-# Setup logging
-log.basicConfig(stream=sys.stdout, level=log.DEBUG)
 
 # Read environment variables
 URL_CLIENT = os.getenv("SERVER_ADDRESS", default="tcp://127.0.0.1:5555")
 SELECTED_CITY = os.getenv("CITY", default="Berlin")
 DB_NAME = os.getenv("DB_NAME", default="messages.db")
+REQUEST_TIMEOUT = os.getenv("REQUEST_TIMEOUT", default=1000)
+REQUEST_RETRIES = os.getenv("REQUEST_RETRIES", default=6)
+CLIENT_ID = os.getenv("CLIENTID", default=random.randint(0, 1000000))
 
+# Setup logging
+log.basicConfig(stream=sys.stdout, level=log.DEBUG)
+
+
+"""Setup DB if not already exists"""
 def setup_db(name):
-    # Setting up DB if not already exists
-    conn = db_helper.create_connection(name)
+    conn = db_helper.create_connection(name)    
     db_helper.setup_table(conn)
     conn.close()
 
+"""Get socket with default settings"""
 def get_socket(address: str):
-    context = zmq.Context() # With asyn to much in parallel
+    context = zmq.Context()
     socket = context.socket(zmq.REQ)
-    socket.setsockopt(zmq.SNDTIMEO, 0)
+    socket.setsockopt(zmq.SNDTIMEO, 1000)
+    socket.setsockopt(zmq.RCVTIMEO, 1000)
     socket.setsockopt(zmq.SNDHWM, 0)
-    socket.setsockopt(zmq.SNDBUF, 10) # Zero means underlying default from OS is used
+    socket.setsockopt(zmq.LINGER, 0)
+    socket.setsockopt(zmq.SNDBUF, 1) # Zero means underlying default from OS is used
     log.info(f"Connecting to server at {address}")
     socket.connect(address)
     return socket
 
+
+"""Periodically clean the DB of sent messages"""
 def db_cleaner():
     conn = db_helper.create_connection(DB_NAME)
     while True:
         db_helper.delete_all_send_message(conn)
         time.sleep(60)
 
+"""
+    Send message to server and handle errors:
+        socket: zmq socket
+        db_conn: sqlite connection
+        message: message to send
+        is_retry: is this a retry message (if yes DB will be updated)
+"""""
+def handle_send_message(socket, message) -> bool:
+    try:
+        # log.info(f"Sending sensor_data - {message}")
+        socket.send_pyobj(message)
+        ack = socket.recv()
+        if ack != b'ACK':
+            raise NoAckException("No ACK received")
+        return True
+    except zmq.error.ZMQError as e:
+        log.warning("Message could not be send, storing in DB")
+        return False
+    except NoAckException as e:
+        log.warning("No ACK received, storing in D")
+        return False
 
+
+"""Check DB for unsend messages and send them"""
 def db_sender():
-    socket = get_socket(URL_CLIENT)
-    conn = db_helper.create_connection(DB_NAME)
-    
     log.info("Connecting to db to check for unsent messages")
+    db_conn = db_helper.create_connection(DB_NAME)
     while True:
-        id, message = db_helper.get_next_unsend_message(conn)
-        if id is not None:
-            try:
-                log.info(f"Sending unsent message with id {id}")
-                socket.send_pyobj(message)
-                ack = socket.recv()
-                if ack != b'ACK':
-                    raise NoAckException("No ACK received")
-                db_helper.mark_message_as_send(conn, id)
-            except zmq.error.ZMQError as e:
-                log.warning("Message could not be send, storing in DB")
-                db_helper.increase_send_attempt(conn, id)
-            except NoAckException as e:
-                log.warning("No ACK received, storing in D")
-                db_helper.increase_send_attempt(conn, id)
-        
-        time.sleep(2)
+        retries = 0
 
-def sender():
-    socket = get_socket(URL_CLIENT)
-    conn = db_helper.create_connection(DB_NAME)
+        socket = get_socket(URL_CLIENT)
+        while retries < REQUEST_RETRIES:
+            id, message = db_helper.get_next_unsend_message(db_conn)
+            if id is not None:
+                success = handle_send_message(socket, message)
+                
+                if success:
+                    db_helper.mark_message_as_send(db_conn, id)
+                    retries = 0
+                else:
+                    db_helper.increase_send_attempt(db_conn, id)
+                    retries += 1
+            
+            time.sleep(0.1)
 
-    count = 0
-    while True:
-        sensor_data = random.choice(sensor_cities).generate_sensor_data()
-        try:
-            log.info(f"Sending sensor_data - {sensor_data}")
-            socket.send_pyobj(sensor_data)
-            ack = socket.recv()
-            if ack != b'ACK':
-                raise NoAckException("No ACK received")
-            count += 1
-        except zmq.error.ZMQError as e:
-            log.warning("Message could not be send, storing in DB")
-            data_json = json.dumps(sensor_data)
-            db_helper.write_unsend_message(conn, data_json)
-        except NoAckException as e:
-            log.warning("No ACK received, storing in D")
-            db_helper.increase_send_attempt(conn, id)
+        socket.close()
+
+"""Send newest incoming sensor data to server"""
+def sender(queue):
+    db_conn = db_helper.create_connection(DB_NAME)
     
-        time.sleep(2)
+    while True:
+        retries = 0
+
+        socket = get_socket(URL_CLIENT)
+        while retries < REQUEST_RETRIES:
+            message = queue.get()
+            success = handle_send_message(socket, message)
+            if success:
+                retries = 0
+            else:
+                data_json = json.dumps(message)
+                db_helper.write_unsend_message(db_conn, data_json)
+                retries += 1
+            time.sleep(0.1)
+
+        socket.close()
+
+
+def data_generator_runner(sensor_generator, queue):
+    for i in range(50):
+        sensor_data = sensor_generator.generate_sensor_data()
+        queue.put(sensor_data)
+        time.sleep(0.5)
+
 
 if __name__ == "__main__":
     log.info("Connecting server...")
@@ -116,14 +153,23 @@ if __name__ == "__main__":
     # Setup DB
     setup_db(DB_NAME)
 
-    sender_thread = threading.Thread(target=sender, args=())
+    # Sensor data queue
+    queue = queue.Queue()
+
+    # Setup Threads
+    data_generator_thread = threading.Thread(target=data_generator_runner, args=(sensor_generator_1, queue))
+    sender_thread = threading.Thread(target=sender, args=(queue, ))
     db_sender_thread = threading.Thread(target=db_sender, args=())
     db_cleaner_thread = threading.Thread(target=db_cleaner, args=())
+    
     sender_thread.start()
+    data_generator_thread.start()
     db_sender_thread.start()
     db_cleaner_thread.start()
 
     sender_thread.join()
+    data_generator_thread.join()
     db_sender_thread.join()
+    db_cleaner_thread.join()
 
     log.info("Server is closing...")
